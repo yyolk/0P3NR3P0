ebc16e573e0cacdac975f7a654dc1ef32fef29d4
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-09 14:13:54 -0600

    working base, shows how to add different fields etc.

--- views/recent-items/map.js
+++ views/recent-items/map.js
@@ -1,6 +1,11 @@
 function(doc) {
   if (doc.created_at) {
-      emit(doc.created_at, {url : doc.url});
+      emit(doc.created_at, {
+          url : doc.url,
+          homepage_url : doc.homepage_url,
+          Description : doc.Description,
+          tags : doc.tags
+      });
   }
 };
 
