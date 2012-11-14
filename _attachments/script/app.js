c344e504bcbf0acabe2b4a24630e37e1086bd63f
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-14 14:48:25 -0600

    beginning separation

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -16,9 +16,42 @@ $(function() {
         return o;
     };
 
+
+    //helper functions
+    function linkify(inputText) {
+        var replaceText, replacePattern1, replacePattern2, replacePattern3;
+
+        //URLs starting with http://, https://, or ftp://
+        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+        //Change email addresses to mailto:: links.
+        replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
+        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+        return replacedText
+    }
+
+    function ValidURL(str) {
+        var pattern = new RegExp('http://[a-z].[a-z].?[a-z].?[a-z]'); // fragment locater
+            if(!pattern.test(str)) {
+                //alert("Please enter a valid URL.");
+                return false;
+            } else {
+                return true;
+            }
+    }
+
+
+
+    //begin meaty couch js code
     var path = unescape(document.location.pathname).split('/'),
-    design = path[3],
-    db = $.couch.db(path[1]);
+        design = path[3],
+        db = $.couch.db(path[1]);
 
     function drawItems() {
         db.view(design + "/recent-items", {
@@ -87,47 +120,14 @@ $(function() {
         var form = this, doc = $(form).serializeObject();
         doc.created_at = new Date();
         if (!checkFields(doc)) { 
-            alert('please insert valid items into the form!');
+            alert('Please insert valid items into the form!');
             //would like this to go directly to the messed up field.
             //$(this).find("input[name=url]").focus();
-            }
+        }
         else{
             db.saveDoc(doc, {success : function() {form.reset();}});}
     }).find("input").focus();
 
-    function findLinks(){
-        $("p.url").each(function() {
-            $(this).html(linkify($(this).text()));
-        });
-
-    }
-    function linkify(inputText) {
-        var replaceText, replacePattern1, replacePattern2, replacePattern3;
-
-        //URLs starting with http://, https://, or ftp://
-        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
-
-        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
-
-        //Change email addresses to mailto:: links.
-        replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
-        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
-
-        return replacedText
-    }
-
-    function ValidURL(str) {
-        var pattern = new RegExp('http://[a-z].[a-z].?[a-z].?[a-z]'); // fragment locater
-        if(!pattern.test(str)) {
-            //alert("Please enter a valid URL.");
-            return false;
-        } else {
-            return true;
-        }
-    }
 
 
 });
