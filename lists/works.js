f23e3029bca77d319d3359032903715a19b16644
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-14 20:36:13 -0600

    attept at getting first _list view; +cycle.html

--- lists/works.js
+++ lists/works.js
@@ -1,15 +1,3 @@
 function(head, req) {
-    var row, key;
-    while (row = getRow()){
-        var post = row.value;
-        key = row.key;
-    send(template(templates.index.row, {
-        url : post.url,
-        homepage_url : post.homepage_url,
-        description: post.description,
-        date : post.created_at,
-        link : showPath('post', row.id)
-    }));
-    }
   
 }
--- shows/post.js
+++ shows/post.js
@@ -1,6 +1,6 @@
 function(doc, req) {  
     return {
-        body : "<style type='text/css'>*{margin:0;padding:0;} iframe { border:none;  width:100%; height:100%;}</style>"+"<iframe src='"+doc.url+"'></iframe>"
+        body : "<iframe src='"+doc.url+"'></iframe>"
 
     }
 }
