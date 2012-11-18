afd4c289f55f501a4e532f776ce2192b1952d9be
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-18 12:58:53 -0600

    title field

--- views/recent-items/map.js
+++ views/recent-items/map.js
@@ -2,6 +2,7 @@ function(doc) {
   if (doc.created_at) {
       emit(doc.created_at, {
           id : doc._id,
+          title: doc.title,
           url : doc.url,
           author: doc.author,
           homepage_url : doc.homepage_url,
