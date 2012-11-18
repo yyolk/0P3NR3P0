30e33409493230008a42c8f2a98afe39ffe7b0c2
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-18 14:52:33 -0600

    works list

--- lists/works.js
+++ lists/works.js
@@ -1,3 +1,22 @@
-function(head, req) {
-  
+function (head, req) {
+    // specify that we're providing a JSON response
+    provides('json', function() {
+        // create an array for our result set
+        var results = [];
+
+        while (row = getRow()) {
+            results.push({
+                id : row.value._id,
+                title: row.value.title,
+                url : row.value.url,
+                author: row.value.author,
+                homepage_url : row.value.homepage_url,
+                description : row.value.description,
+                tags : row.value.tags
+            });
+        }
+
+        // make sure to stringify the results :)
+        return(JSON.stringify(results));
+    });
 }
--- views/by_tags/map.js
+++ views/by_tags/map.js
@@ -3,14 +3,29 @@ function(doc) {
 
     var emit_sequence = function(base, disp) {
         if (disp.length > 1) {
-            emit(base.concat(disp[0]), 1);
+            emit(base.concat(disp[0]), {
+          id : doc._id,
+          title: doc.title,
+          url : doc.url,
+          author: doc.author,
+          homepage_url : doc.homepage_url,
+          description : doc.description,
+          tags : doc.tags
+      });
             emit_sequence(base.concat(disp[0]), disp.slice(1, disp.length));
             emit_sequence(base, disp.slice(1, disp.length));
         } else if (disp.length == 1) {
-            emit(base.concat(disp[0]), 1);
+            emit(base.concat(disp[0]), {
+          id : doc._id,
+          title: doc.title,
+          url : doc.url,
+          author: doc.author,
+          homepage_url : doc.homepage_url,
+          description : doc.description,
+          tags : doc.tags
+      });
         }
     }
 
     emit_sequence([], doc.tags);
 }
-
