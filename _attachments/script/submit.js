1ba6fa59eab6a9df527798872f60f45cc0505f51
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-28 16:14:05 -0600

    merging

--- _attachments/script/submit.js
+++ _attachments/script/submit.js
@@ -128,33 +128,34 @@ $(function() {
 
   var path = unescape(document.location.pathname).split('/'),
       design = path[3],
-      db = $.couch.db(path[1]);
+      name =  'openrepo/_design/0P3NR3P0',
+      db = $.couch.db(name);
 
-  function drawItems() {
-    db.view(design + "/recent-items", {
-      descending : "true",
-      limit : 50,
-      update_seq : true,
-      success : function(data) {
-        setupChanges(data.update_seq);
-        var them = $.mustache($("#recent-links").html(), {
-          items : data.rows.map(function(r) {return r.value;})
-        });
-        $("#content").html(them);
-      }
-    });
-
-  };
-  drawItems();
-  var changesRunning = false;
-
-  function setupChanges(since) {
-    if (!changesRunning) {
-      var changeHandler = db.changes(since);
-      changesRunning = true;
-      changeHandler.onChange(drawItems);
-    }
-  }
+//  function drawItems() {
+//    db.view(design + "/recent-items", {
+//      descending : "true",
+//      limit : 50,
+//      update_seq : true,
+//      success : function(data) {
+//        setupChanges(data.update_seq);
+//        var them = $.mustache($("#recent-links").html(), {
+//          items : data.rows.map(function(r) {return r.value;})
+//        });
+//        $("#content").html(them);
+//      }
+//    });
+//
+//  };
+//  drawItems();
+//  var changesRunning = false;
+//
+//  function setupChanges(since) {
+//    if (!changesRunning) {
+//      var changeHandler = db.changes(since);
+//      changesRunning = true;
+//      changeHandler.onChange(drawItems);
+//    }
+//  }
   $("#create-link").submit(function(e){
     e.preventDefault();
     var form = this, doc = $(form).serializeObject();
