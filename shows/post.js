888dea8c1283a0c41745876b23359f134300ef96
yyolk <input.output@gmail.com>
2013-03-18 21:05:22 -0500

    mondays

--- shows/post.js
+++ shows/post.js
@@ -9,6 +9,14 @@ function(doc, req) {
       return false;
     }
   }
+  function checkFB(str){
+    var pattern = new RegExp(/^https:\/\/www\.facebook\.com.*$/i);
+    if (pattern.test(str)){
+      return true;
+    } else{
+      return false;
+    }
+  }
   if(checkImg(doc.url)){
     return {
       code : 200,
@@ -18,13 +26,14 @@ function(doc, req) {
        },
       body :  '<head><link rel="stylesheet" href="/openrepo/_design/0P3NR3P0/style/post.css" type="text/css"></link></head><body style="'+"background-image:url('" + doc.url +"');"+'"'+"></body>"
     };
+  } else if(checkFB(doc.url)){
+    return { code : 302, headers : { "Access-Control-Allow-Origin": "*", "Location" : doc.url } };
+
   } else {
-    // return { code : 302, headers : { "Access-Control-Allow-Origin": "*", "Location" : doc.url } };
     return { code : 200, headers : { 
       "Access-Control-Allow-Origin": "*", 
       "Content-Type:" : "text/html"   },
       body: '<html><head><link rel="stylesheet" href="/openrepo/_design/0P3NR3P0/style/post.css" type="text/css"></link></head><body><iframe src="'+doc.url+'"></iframe></body></html>'
        };
-
   }
 }
