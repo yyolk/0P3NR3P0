699ce20dd5442260288a2cf0f53c15abe3f0223d
yyolk <input.output@gmail.com>
2013-03-18 19:56:42 -0500

    mondays

--- shows/post.js
+++ shows/post.js
@@ -19,6 +19,12 @@ function(doc, req) {
       body :  '<head><link rel="stylesheet" href="/openrepo/_design/0P3NR3P0/style/post.css" type="text/css"></link></head><body style="'+"background-image:url('" + doc.url +"');"+'"'+"></body>"
     };
   } else {
-    return { code : 301, headers : { "Access-Control-Allow-Origin": "*", "Location" : doc.url } };
+    // return { code : 302, headers : { "Access-Control-Allow-Origin": "*", "Location" : doc.url } };
+    return { code : 200, headers : { 
+      "Access-Control-Allow-Origin": "*", 
+      "Content-Type:" : "text/html"   },
+      body: '<html><head><link rel="stylesheet" href="/openrepo/_design/0P3NR3P0/style/post.css" type="text/css"></link></head><body><iframe src="'+doc.url+'"></iframe></body></html>'
+       };
+
   }
 }
