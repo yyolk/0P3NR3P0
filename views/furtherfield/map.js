2c562c186401d4236c082aff64067473a84c3261
yyolk <input.output@gmail.com>
2013-05-06 16:18:54 -0500

    merge

---
+++ views/furtherfield/map.js
@@ -0,0 +1,13 @@
+function(doc) {
+  if (doc.tags[0]=='furtherfieldgallery') {
+      emit(doc.author, {
+          id : doc._id,
+          title: doc.title,
+          url : doc.url,
+          author: doc.author,
+          homepage_url : doc.homepage_url,
+          description : doc.description,
+          tags : doc.tags
+      });
+  }  
+}
