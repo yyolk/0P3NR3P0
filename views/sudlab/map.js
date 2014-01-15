b8a930b1238ea8d023ca7fe023120352d162d691
Joseph Yølk Chiocchi <joe@yolk.cc>
2014-01-15 15:53:52 -0600

    new stuffs

---
+++ views/sudlab/map.js
@@ -0,0 +1,14 @@
+function(doc) {
+  if (doc.show=='sudlab') {
+      emit(doc.author, {
+          id : doc._id,
+          title: doc.title,
+          url : doc.url,
+          author: doc.author,
+          homepage_url : doc.homepage_url,
+          description : doc.description,
+          created_at : doc.created_at,
+          tags : doc.tags
+      });
+  }  
+}
