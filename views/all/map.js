0194d3b9d6d7f72c4d4ab73af96d8c746c57f72a
Joseph Yølk Chiocchi <joe@yolk.cc>
2014-07-31 14:44:35 -0500

    use all view

--- views/all/map.js
+++ views/all/map.js
@@ -1,13 +1,14 @@
 function(doc) {
   if (doc.created_at) {
     emit(doc._id, {
+      id : doc._id,
       title: doc.title,
-      url: doc.url,
+      url : doc.url,
       author: doc.author,
-      homepage_url: doc.homepage_url,
-      description: doc.description,
-      email: doc.email,
-      tags: doc.tags
+      homepage_url : doc.homepage_url,
+      description : doc.description,
+      created_at : doc.created_at,
+      tags : doc.tags
     });
   }
 }
