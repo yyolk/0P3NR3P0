a7a920e58be2645bcd8c448fe5be609f55ff26ff
Joseph Yølk Chiocchi <joe@yolk.cc>
2014-08-06 18:34:22 -0500

    add glitchicago view

---
+++ views/glitChicago/map.js
@@ -0,0 +1,14 @@
+function(doc) {
+  if (doc.show=='glitChicago') {
+      emit(-doc.created_at, {
+          id : doc._id,
+          title: doc.title,
+          url : doc.url,
+          author: doc.author,
+          homepage_url : doc.homepage_url,
+          description : doc.description,
+          created_at : doc.created_at,
+          tags : doc.tags
+      });
+  }  
+}
