95714ccf04f34fba143744039cbf52175108c0c9
Joseph Yølk Chiocchi <input.output@gmail.com>
2015-03-01 17:22:22 -0600

    use a data attribute

--- lists/show.js
+++ lists/show.js
@@ -45,17 +45,17 @@ function (head, req) {
                 'author': 'Testing Bot'
             },
             {
-                'id': '2',
+                'id': '2d737897c814468aaf0989a49805b017',
                 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis scelerisque dolor, feugiat cursus eros malesuada id. Nullam porta, erat in euismod molestie, turpis tellus pharetra augue, commodo consequat justo felis quis mauris. Donec enim urna, ultrices vel nisi eu, pretium eleifend ex. Cras quis laoreet tortor, quis fermentum ipsum. Sed bibendum maximus ex eu tincidunt. Pellentesque ac ligula nec velit aliquet aliquet vitae ut nunc. Integer imperdiet odio pharetra, porta lectus id, gravida diam. Duis et justo hendrerit, tristique ipsum sit amet, aliquet ex. Nullam eros velit, vulputate in fringilla et, pretium sit amet justo. Aliquam malesuada iaculis mauris, id fringilla magna vehicula a. Nulla suscipit tortor mi, vitae pretium elit dapibus eu. Phasellus consequat ex at iaculis condimentum. Pellentesque molestie molestie massa et facilisis. Integer risus est, finibus eget blandit a, interdum et massa. Donec fringilla vulputate vehicula. Sed est eros, posuere ac porttitor sit amet, sollicitudin blandit sem.',
                 'author': 'Testing Bot'
             },
             {
-                'id': '32',
+                'id': '2d737897c814468aaf0989a49805b017',
                 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis scelerisque dolor, feugiat cursus eros malesuada id. Nullam porta, erat in euismod molestie, turpis tellus pharetra augue, commodo consequat justo felis quis mauris. Donec enim urna, ultrices vel nisi eu, pretium eleifend ex. Cras quis laoreet tortor, quis fermentum ipsum. Sed bibendum maximus ex eu tincidunt. Pellentesque ac ligula nec velit aliquet aliquet vitae ut nunc. Integer imperdiet odio pharetra, porta lectus id, gravida diam. Duis et justo hendrerit, tristique ipsum sit amet, aliquet ex. Nullam eros velit, vulputate in fringilla et, pretium sit amet justo. Aliquam malesuada iaculis mauris, id fringilla magna vehicula a. Nulla suscipit tortor mi, vitae pretium elit dapibus eu. Phasellus consequat ex at iaculis condimentum. Pellentesque molestie molestie massa et facilisis. Integer risus est, finibus eget blandit a, interdum et massa. Donec fringilla vulputate vehicula. Sed est eros, posuere ac porttitor sit amet, sollicitudin blandit sem.',
                 'author': 'Testing Bot'
             },
             {
-                'id': '24',
+                'id': '2d737897c814468aaf0989a49805b017',
                 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis scelerisque dolor, feugiat cursus eros malesuada id. Nullam porta, erat in euismod molestie, turpis tellus pharetra augue, commodo consequat justo felis quis mauris. Donec enim urna, ultrices vel nisi eu, pretium eleifend ex. Cras quis laoreet tortor, quis fermentum ipsum. Sed bibendum maximus ex eu tincidunt. Pellentesque ac ligula nec velit aliquet aliquet vitae ut nunc. Integer imperdiet odio pharetra, porta lectus id, gravida diam. Duis et justo hendrerit, tristique ipsum sit amet, aliquet ex. Nullam eros velit, vulputate in fringilla et, pretium sit amet justo. Aliquam malesuada iaculis mauris, id fringilla magna vehicula a. Nulla suscipit tortor mi, vitae pretium elit dapibus eu. Phasellus consequat ex at iaculis condimentum. Pellentesque molestie molestie massa et facilisis. Integer risus est, finibus eget blandit a, interdum et massa. Donec fringilla vulputate vehicula. Sed est eros, posuere ac porttitor sit amet, sollicitudin blandit sem.',
                 'author': 'Testing Bot'
             },
