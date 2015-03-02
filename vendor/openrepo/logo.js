92e59a9958cdb0427c86380ee73c80f858a70bfa
Joseph Yølk Chiocchi <joe@yolk.cc>
2015-03-02 16:42:03 -0600

    or use logo

--- vendor/openrepo/logo.js
+++ vendor/openrepo/logo.js
@@ -2,10 +2,10 @@ exports.getShowLogo = function(name) {
   logoMap = {
       'netartizens': 'netartizens.png',
       'furtherfield': 'furtherfield_logo.png',
-      'furtherfieldgallery': 'furtherfield_logo.png',
+      'furtherfieldgallery': 'furtherfield_logo2.png',
       'glitChicago': 'glitchicago.jpg',
       'sudlab': 'sudlablogo.png',
       'mcainternetsuperheroes': 'mca.gif',
   }
-  return logoMap[name]
+  return logoMap[name] || '0.png'
 }
