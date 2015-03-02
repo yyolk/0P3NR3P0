b94e756dbc697e842d9d49e0c8bc67a04a042cbd
Joseph Yølk Chiocchi <input.output@gmail.com>
2015-03-01 19:14:18 -0600

    add more logos

--- vendor/openrepo/logo.js
+++ vendor/openrepo/logo.js
@@ -1,8 +1,10 @@
 exports.getShowLogo = function(name) {
   logoMap = {
-      'netartizens': "netartizens.png",
-      'furtherfield': "furtherfield_logo.png",
-      'glitChicago': "glitchicago.jpg"
+      'netartizens': 'netartizens.png',
+      'furtherfield': 'furtherfield_logo.png',
+      'glitChicago': 'glitchicago.jpg',
+      'sudlab': 'sudlablogo.png',
+      'mca': 'mca.gif'
   }
   return logoMap[name]
 }
